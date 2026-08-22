f61b8c6905b27e9dacdbad2200a000a185a1f404
ThiagoFukunaga <thiago.fukunaga@sptech.school>
2026-08-22 03:53:02 -0300

    feat: adicionando tela de listagem de pacientes

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import CadastroPage from './pages/Cadastro'; 
 import LoginPage from './pages/Login/Login'; 
+import PatientList from './components/PatientList';
+
 
 import './App.css';
 
@@ -11,6 +13,7 @@ function App() {
       <Routes>
         {/* se entrar na root (localhost:5173/) vai pro cadastro */}
         <Route path="/" element={<Navigate to="/cadastro" />} />
+        <Route path="/pacientes" element={<PatientList />} />  
 
         {/* rotas da app para cada elemento do front */}
         <Route path="/cadastro" element={<CadastroPage />} />
